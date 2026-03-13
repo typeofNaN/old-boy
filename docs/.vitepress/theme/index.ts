0e9e389e5dc8d6cc2699c544d40ed5dd7961ccd4
typeofNaN <dmdefine6@gmail.com>
2026-03-13 12:26:08 +0800

    fix: 修复移动端彩虹动画 🐞

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -33,13 +33,13 @@ export default {
 if (typeof window !== 'undefined') {
   // detect browser, add to class for conditional styling
   const ua = navigator.userAgent.toLowerCase()
-  const isMobile = /mobi|android|iphone|ipad|ipod/i.test(ua)
+  // const isMobile = /mobi|android|iphone|ipad|ipod/i.test(ua)
 
-  if (!isMobile) {
-    if (ua.includes('chrome')) document.documentElement.classList.add('browser-chrome')
-    else if (ua.includes('firefox')) document.documentElement.classList.add('browser-firefox')
-    else if (ua.includes('safari')) document.documentElement.classList.add('browser-safari')
-  }
+  // if (!isMobile) {
+  if (ua.includes('chrome')) document.documentElement.classList.add('browser-chrome')
+  else if (ua.includes('firefox')) document.documentElement.classList.add('browser-firefox')
+  else if (ua.includes('safari')) document.documentElement.classList.add('browser-safari')
+  // }
 }
 
 // Speed up the rainbow animation on home page
